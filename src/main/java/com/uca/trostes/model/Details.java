4d82cb66d5751ed91e6cba2b3432b90bf8591c55
andreayalad <alejandrayala01@gmail.com>
2022-05-10 22:37:45 -0600

    models

--- src/main/java/com/uca/trostes/model/Details.java
+++ src/main/java/com/uca/trostes/model/Details.java
@@ -4,8 +4,15 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,12 +26,24 @@ public class Details implements Serializable{
 	@Id
 	@Column(name = "ID_DETAIL")
 	Integer idDetail;
-	@Column(name = "ID_TEACHER")
-	Integer idTeacher;
 	@Column(name = "GROUP")
 	Integer group;
 	@Column(name = "CLASSROOM")
 	Integer classroom;
 	@Column (name = "SIGNATURE")
 	String signature;
+	 @OneToOne(fetch = FetchType.LAZY)
+	    @JsonIgnore
+	    @JoinColumns({@JoinColumn(name = "ID_TEACHER", referencedColumnName = "ID_TEACHER")})
+	    Teacher teacher;
+	 
+	 /**
+	    * Metodo para obtener la llave primaria del padre de la entidad
+	    * @return el tipo de dato de la llave primaria
+	    * @author Generador-Safi
+	    * @version 1.0
+	    **/
+	 public    Integer getTeacherToDetails(){
+	        return (this.teacher== null) ? null : this.teacher.getIdTeacher();
+	    }
 }
